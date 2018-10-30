e191375f773fc041fabfa4d8de0a1dda3a58c00c
shulkmaster <00088116@uca.edu.sv>
2018-10-29 23:11:32 -0600

    Revirtiendo el commit que se cago en todo
    Revirtiendo el commit que se cago en todo, eso y un componente estaba mal creado ERROR in src/app/HomeScreen/HomeScreen.component.ts(6,3): error TS1146: Declaration expected.

--- src/app/HomeScreen/HomeScreen.component.ts
+++ src/app/HomeScreen/HomeScreen.component.ts
@@ -1,6 +1,17 @@
-import{ Component } from '@angular/core';
+import{ Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'HomeSceen',
     template: 'HomeScreen.component.html'
 })
+export class HomeScreenComponent implements OnInit {
+
+    constructor() {
+
+    }
+    ngOnInit() {
+    }
+  
+  
+  
+  }
