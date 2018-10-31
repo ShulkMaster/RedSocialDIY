5c869bd697bb9f0e7ad30c665a79b1c8f4bd8168
JorellanaF <00103717@uca.edu.sv>
2018-10-31 14:15:32 -0600

    se importo RegistroComponent en routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { InforComponent } from './infor/infor.component';
 import { GaleryComponent } from './galery/galery.component';
 import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'infor', component: InforComponent},
   { path: 'galery', component: GaleryComponent},
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path 'registro', component: RegistroComponent}
 ];
 
 @NgModule({
