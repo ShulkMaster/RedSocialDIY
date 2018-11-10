d3fe1a54227e2bcb579994b508216c7d7b799956
DiegoVeega <00148816@uca.edu.sv>
2018-11-10 15:44:47 -0600

    Funcionalidad de directivas ng para header de pagina

--- src/app/navegacion/navegacion.component.ts
+++ src/app/navegacion/navegacion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../servicios/auth.service';
+import { usuario } from '../classes/usuario';
 
 @Component({
   selector: 'app-navegacion',
@@ -7,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavegacionComponent implements OnInit {
 
-  somevalue: string = 'xd';
-  constructor() { }
+  user: usuario;
+  
+  constructor(private loger: AuthService) {
+    this.user = loger.myUser;
+    console.log('Home constructor fired', this.user);
+  }
 
   ngOnInit() {
+    if(this.user){
+      console.log(this.user)
+    }
   }
 
 }
