4822b44a1e5442194aa4bbd91b95b1efee41f7ba
shulkmaster <00088116@uca.edu.sv>
2018-11-10 22:13:02 -0600

    Modificando identacion del codigo
    Modificando identacion del codigo

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,29 +11,29 @@ import { AuthService } from '../servicios/auth.service';
 export class LoginComponent implements OnInit {
 
   formlogin: FormGroup;
-  submited: boolean = false; // Este booleano se usa para hacer cambiso en el css si se envia y fallan las validadciones
-  exito: boolean = false; //este booleano de aqui es el que controla si la app pasa a la siguiente ruta o no
+  submited = false; // Este booleano se usa para hacer cambiso en el css si se envia y fallan las validadciones
+  exito = false; // este booleano de aqui es el que controla si la app pasa a la siguiente ruta o no
 
-  constructor(private formmaker: FormBuilder, private uservalidator: AuthService,) { 
+  constructor(private formmaker: FormBuilder, private uservalidator: AuthService) {
     this.formlogin = this.formmaker.group({
-      "username": ['', Validators.required],
-      "passwd": ['', Validators.required]
+      'username': ['', Validators.required],
+      'passwd': ['', Validators.required]
     });
   }
 
   ngOnInit() {
   }
 
-  onSubmit(){
+  onSubmit() {
     this.submited = true;
     this.uservalidator.isUser(this.pickdata());
   }
 
-  pickdata(){
-    let datapackage = {
+  pickdata() {
+    const datapackage = {
       username: this.formlogin.controls.username.value,
       passwd: this.formlogin.controls.passwd.value
-    }
+    };
     return datapackage;
   }
 }
