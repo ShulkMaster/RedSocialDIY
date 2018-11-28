b743810f3e8d18489a0dd58caa1ece18703e853a
DiegoVeega <00148816@uca.edu.sv>
2018-11-28 01:44:14 -0600

    Mejora estetica de red social, modificacion de componentes, cambios en CSS, intento de mejorar interaccion con usuario

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from '../data.service';
+import { AuthService } from '../servicios/auth.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  running = false;
+
+  constructor(public session: AuthService, public dataserver: DataService) {
+    console.log('Home constructor fired from home');
+    this.session = session;
+    this.dataserver = dataserver;
+  }
 
   ngOnInit() {
+  }
+
+  checkuserage() {
+    if (this.session.myUser.edad >= 10) {
+      console.log('su usuario si es mayor de edad', this.session.myUser);
+      return true;
+    }
+    return false;
   }
 
 }
