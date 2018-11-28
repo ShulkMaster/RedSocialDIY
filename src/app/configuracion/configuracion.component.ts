3ef6310f93e58f1ac03ea760830ca7ada6782f41
00095417 <00095417@uca.edu.sv>
2018-11-28 11:45:09 -0600

    solucionado el que aparescan los campos a modificar de la pagina configuracion pero aun faltan campos a agregar

--- src/app/configuracion/configuracion.component.ts
+++ src/app/configuracion/configuracion.component.ts
@@ -15,10 +15,48 @@ export class ConfiguracionComponent implements OnInit {
   }
 
   clickConfigGeneral(){
-    console.log("estoy en configuracion general");
+    let itemGe = document.getElementById("general"),
+        itemPriv = document.getElementById("privacidad");
+
+    itemGe.classList.remove("d-none");
+    itemGe.classList.add("d-block");
+    itemPriv.classList.remove("d-block");
   }
   clickConfigPrivado(){
-    console.log("estoy en la configuracion de la privacidad");
+    let itemGe = document.getElementById("general"),
+        itemPriv = document.getElementById("privacidad");
+    
+      itemGe.classList.add("d-none");
+      itemGe.classList.remove("d-block");
+      itemPriv.classList.add("d-block");
+  }
+  opCampoGe(opc){
+    
+    let itemNewName = document.getElementById('campoNewName'),
+        itemNewPass = document.getElementById('campoNewPass'),
+        itemNewTema = document.getElementById('campoNewTema');
+    switch(opc){
+      case 0:
+        itemNewName.classList.remove("d-block");
+        itemNewPass.classList.remove("d-block");
+        itemNewTema.classList.remove("d-block");
+        break;
+      case 1:
+        itemNewName.classList.add("d-block");
+        itemNewPass.classList.remove("d-block");
+        itemNewTema.classList.remove("d-block");
+        break;
+      case 2:
+        itemNewPass.classList.add("d-block");
+        itemNewName.classList.remove("d-block");
+        itemNewTema.classList.remove("d-block");
+        break;
+      case 3:
+        itemNewTema.classList.add("d-block");
+        itemNewName.classList.remove("d-block");
+        itemNewPass.classList.remove("d-block");
+        break;
+    }
   }
 
 }
