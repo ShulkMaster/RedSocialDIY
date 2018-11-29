b5fb8f7268306542e834ffc2fa14468a2222a481
Yury <shulkmaster@gmail.com>
2018-11-29 16:34:08 -0600

    Adding basic controls to the editor

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ViewChild, Renderer2, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer2, Input } from '@angular/core';
 import { PublicacionComponent } from '../publicacion/publicacion.component';
 
 @Component({
@@ -7,11 +7,14 @@ import { PublicacionComponent } from '../publicacion/publicacion.component';
   styleUrls: ['./editor.component.css']
 })
 
-export class EditorComponent implements OnInit, AfterViewInit {
+export class EditorComponent implements OnInit {
 
   @ViewChild(PublicacionComponent) pubilicat: PublicacionComponent;
-  @Input() superstring = '';
+  superstring = '';
   slectedIndex: number;
+  editing = false;
+  listeners = [];
+
 
   constructor(private renderer2: Renderer2) { }
 
@@ -19,26 +22,31 @@ export class EditorComponent implements OnInit, AfterViewInit {
     this.pubilicat.onEdit = true;
   }
 
-  ngAfterViewInit(): void {
-    this.startEdit();
-  }
-
-
   private startEdit() {
-    console.log('cosa rara');
-    const algoraro = this.pubilicat.cuadropublic.nativeElement.querySelectorAll('.parrafo > p');
-    console.log('modo edicion activado', algoraro);
-    algoraro.forEach( (node, indec) => {
-      this.renderer2.listen(node, 'click', evt => {
-        algoraro.forEach(element => {
-          this.renderer2.removeClass(element, 'onedit');
-        });
-        console.log('Me diste clik', indec);
-        this.renderer2.addClass(node, 'onedit');
-        this.slectedIndex = indec;
-        this.superstring = node.innerHTML;
+    if (this.editing) {
+      this.editing = false;
+      // auto save!! code here
+      this.listeners.forEach( elem => {
+        elem();
       });
-    });
+      this.listeners = [];
+    } else {
+      this.editing = true;
+      console.log('cosa rara');
+      const algoraro = this.pubilicat.cuadropublic.nativeElement.querySelectorAll('.parrafo > p');
+      console.log('modo edicion activado', algoraro);
+      algoraro.forEach((node, indec) => {
+        this.listeners.push(this.renderer2.listen(node, 'click', evt => {
+          algoraro.forEach(element => {
+            this.renderer2.removeClass(element, 'onedit');
+          });
+          console.log('Me diste clik', indec);
+          this.renderer2.addClass(node, 'onedit');
+          this.slectedIndex = indec;
+          this.superstring = node.innerHTML;
+        }));
+      });
+    }
   }
 
   updatevalue() {
