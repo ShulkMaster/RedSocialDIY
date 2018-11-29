907f063c098847006a1d7a8510aa68856ee282eb
Yury <shulkmaster@gmail.com>
2018-11-29 12:27:18 -0600

    Arreglando corrección del scroll y eventos despues del registro
    Ahora al registrarse la pagina no se queda sin hacer nada, muestra un boton para ir al login si el usuario fue correcto si no muestra un panel con los errores debajo del formulario

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -17,6 +17,9 @@ export class RegistroComponent implements OnInit {
   barwid: number;
   colorPíker: Color;
   lecolor: string;
+  submited: boolean;
+  exito: boolean;
+  mensaje: string;
 
   constructor(private userSuscriber: AuthService, private maker: FormBuilder) {
     this.formregis = this.maker.group({
@@ -45,16 +48,29 @@ export class RegistroComponent implements OnInit {
     this.formregis.controls.passwd.valueChanges.subscribe(
       valor => this.formregis.controls.conpasswd.updateValueAndValidity()
     );
+    this.exito = false;
   }
 
   ngOnInit() {
     this.barwid = 0;
     this.lecolor = Color.geterrorscale(this.barwid);
+    this.submited = false;
   }
 
   registrar() {
+    this.submited = true;
     console.log('ok registrando...', this.pickdata());
-    this.userSuscriber.resgisterUser(this.pickdata());
+    this.userSuscriber.resgisterUser(this.pickdata()).subscribe( (data: any) => {
+      console.log(data, 'gotten from server on register');
+      this.exito = data.status;
+      this.mensaje = data.status ?  'Su usuario fue creado correctamente' : 'Error al crear el usuario';
+    },
+    error => {
+      console.log(error);
+      this.exito = false;
+      this.mensaje = error;
+    }
+  );
   }
 
   private pickdata() {
